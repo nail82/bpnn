b79a55a0eda3f0af1aeba57cc6a0bf361a610a09
bubba <bubba@tbolt>
2012-10-09 20:30:57 -0500

    Squashing funcs implemented.

--- squash_funcs.py
+++ squash_funcs.py
@@ -2,3 +2,17 @@
 This module implements squashing functions
 and first derivatives for the squashing functions.
 """
+
+import numpy as np
+
+def bipolar(x):
+    return (1 - np.exp(-x)) / (1 + np.exp(-x))
+
+def bipolar_prime(x):
+    return (2. * np.exp(x)) / (1 + np.exp(x)**2)
+
+def binary(x):
+    return 1./(1+np.exp(-x))
+
+def binary_prime(x):
+    return np.exp(x) / (1 + np.exp(x)**2)
