0defe5588f8ccaf14886f25d9c530df297cff3bc
bubba <bubba@tbolt>
2012-10-09 21:58:58 -0500

    Sketch of interface to the net.

--- bpnn.py
+++ bpnn.py
@@ -6,9 +6,12 @@ the neural network code.
 from __future__ import print_function
 import sys
 import os
+import neural_net as nn
+import squash_functs as sf
 
 def main():
-    pass
+    mynet = nn.NeuralNet(sf.binary, sf.binary_prime)
+
 
 if __name__ == '__main__':
     main()
