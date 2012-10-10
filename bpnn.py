a5dd95a911390b2781131ff3d6982fa947361ae1
bubba <bubba@tbolt>
2012-10-09 19:59:06 -0500

    Intial import of bpnn executable.

---
+++ bpnn.py
@@ -0,0 +1,14 @@
+"""
+This module is the main entry point to exercise
+the neural network code.
+"""
+
+from __future__ import print_function
+import sys
+import os
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
