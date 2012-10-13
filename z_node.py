26ab6f1ae42934dc82d82f35f54dc840bbc541fe
bubba <bubba@tbolt>
2012-10-13 05:20:46 -0500

    Znode calc_output working.

--- z_node.py
+++ z_node.py
@@ -6,12 +6,14 @@ Fall 2012
 Assignment 3
 
 File: z_node.py
+
+This file implements a hidden node.
 """
 
 class Znode(object):
     """This class models a hidden node in a neural network."""
 
-    def __init__(self, id, squash, squash_prime, dt):
+    def __init__(self, node_id, squash, squash_prime, dt):
         """
         Initialize the node.
         Args:
@@ -21,19 +23,19 @@ class Znode(object):
         squash_prime: First derivative of the squashing function
         dt: The network data table.
         """
-        self.id = id
+        self.id = node_id
         self.squash = squash
         self.squash_prime = squash_prime
         self.dt = dt
 
-    def calc_output():
+    def calc_output(self):
         """
         Calculate this node's output and store the result
         in the data table.
         """
-        input_vec = dt.get_input_vec()
-        v_wts = dt.get_input_to_hidden(self.id)
+        input_vec = self.dt.get_input_vec()
+        v_wts = self.dt.get_input_to_hidden(self.id)
         net_in = input_vec.T * v_wts
-        out = squash(net_in)
-        dt.set_net_in_z(self.id, net_in)
-        dt.set_z_out(self.id, out)
+        out = self.squash(net_in)
+        self.dt.set_net_in_z(self.id, net_in)
+        self.dt.set_z_out(self.id, out)
