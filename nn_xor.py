52f3d5c34ee7f372679e5eeb6d0f83c0fe9f4fba
tsatcher <tsatcher@15.3.1.10.in-addr.arpa>
2012-10-13 22:02:44 -0500

    Import executable to train for xor.

---
+++ nn_xor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+"""
+This exercise sets up and trains the neural network
+to calculate xor on two inputs.
+"""
+
+from __future__ import print_function
+import sys
+import os
+import neural_net as nn
+import squash_funcs as sf
+import numpy as np
+
+def main():
+    alpha = 0.5
+    data = np.matrix("0. 0. 0.; 0. 1. 1.; 1. 1. 0.; 1. 0. 1.")
+    theta = 1e-3
+    iter_limit = 1000
+    total_count = 0
+    mynet = nn.NeuralNet((2, 2, 1), sf.binary, sf.binary_prime, alpha)
+    n = data.shape[0]
+    k = -1
+    while True:
+        total_count += 1
+        k = (k+1) % n
+        pattern = np.asarray(data[k,1:]).squeeze()
+        teacher = np.asarray(data[k,0]).squeeze()
+        error = mynet.train(pattern, teacher, theta)
+        print('Error=>', error)
+        if (total_count > iter_limit) or (error < theta):
+            break
+
+    mynet.prettyprint()
+
+
+if __name__ == '__main__':
+    main()
